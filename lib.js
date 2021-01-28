58d624f0d5df1ac620164c9d7545e08dfb72ec98
ProgramistaZpolski <piotrbadelek@protonmail.com>
2021-01-28 20:15:32 +0100

    Create lib.js

---
+++ lib.js
@@ -0,0 +1,12 @@
+function dataShow() {
+	document.querySelectorAll("[data-show]").forEach(elem => {
+		const result = eval(elem.dataset.show);
+		const display = elem.dataset.showmode ?? "block";
+		if (result) {
+			elem.style.display = display;
+		} else {
+			elem.style.display = "none";
+		}
+		return result;
+	});
+};
